a9dcbe347acbf7f6905399c1f76c2bf71c062599
Sergio Merino <sergio.merino.a@gmail.com>
2021-04-04 13:29:15 +0100

    Save files to directory

--- tools/create_dataset.py
+++ tools/create_dataset.py
@@ -11,15 +11,21 @@ PIN = board.D18
 PIXELS_ORDER = neopixel.RGB
 OFF = (0, 0, 0)
 GREEN = (0, 255, 0)
+IMG_DIR = 'images'
 
 camera = Camera()
 pixels = neopixel.NeoPixel(pin=PIN, n=SIZE, pixel_order=PIXELS_ORDER)
 pixels.fill(OFF)
 
+if not os.path.exists(IMG_DIR):
+    os.makedirs(IMG_DIR)
+
+execution_id = binascii.b2a_hex(os.urandom(3)).decode()
+path_template = f'{IMG_DIR}/light_{execution_id}'
+
 for i in range(SIZE):
-    img_id = binascii.b2a_hex(os.urandom(3)).decode()
-    camera.frame_to_file(f'light_{img_id}_off.png')
+    camera.frame_to_file(f'{path_template}_{i}_off.png')
     pixels[i] = GREEN
     on_frame = camera.get_frame()
-    camera.frame_to_file(f'light_on_{img_id}_on.png')
+    camera.frame_to_file(f'{path_template}_{i}_on.png')
     pixels[i] = OFF
