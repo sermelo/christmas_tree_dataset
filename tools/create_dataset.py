11a88568802f9c552399c352e5b507e0821ec59b
Sergio Merino <sergio.merino.a@gmail.com>
2021-04-04 13:43:23 +0100

    Split the code in 2 methods

--- tools/create_dataset.py
+++ tools/create_dataset.py
@@ -13,22 +13,27 @@ OFF = (0, 0, 0)
 GREEN = (0, 255, 0)
 IMG_DIR = 'images'
 
+def capture_frames(camera, pixels, path_template, pos):
+    frame_captured = False
+    while not frame_captured:
+        camera.frame_to_file(f'{path_template}_{pos}_off.png')
+        exposure_time_off = camera.get_exposure_speed()
+        pixels[pos] = GREEN
+        on_frame = camera.get_frame()
+        camera.frame_to_file(f'{path_template}_{pos}_on.png')
+        exposure_time_on = camera.get_exposure_speed()
+        pixels[pos] = OFF
+        frame_captured = exposure_time_on == exposure_time_off
+
+
 def create_dataset_series(pixels):
     camera = Camera()
     execution_id = binascii.b2a_hex(os.urandom(3)).decode()
     path_template = f'{IMG_DIR}/light_{execution_id}'
 
     for i in range(SIZE):
-        frame_captured = False
-        while not frame_captured:
-            camera.frame_to_file(f'{path_template}_{i}_off.png')
-            exposure_time_off = camera.get_exposure_speed()
-            pixels[i] = GREEN
-            on_frame = camera.get_frame()
-            camera.frame_to_file(f'{path_template}_{i}_on.png')
-            exposure_time_on = camera.get_exposure_speed()
-            pixels[i] = OFF
-            frame_captured = exposure_time_on == exposure_time_off
+        capture_frames(camera, pixels, path_template, i)
+
 
 if not os.path.exists(IMG_DIR):
     os.makedirs(IMG_DIR)
